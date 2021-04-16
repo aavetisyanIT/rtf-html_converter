16c3866a54cd6cf4134029531476c4de51a1c41a
aavetisyanit <aavetisyanit@gmail.com>
2021-04-15 19:00:05 -0500

    Set up Cloud mersive Convert Api

--- App.js
+++ App.js
@@ -1,1 +1,27 @@
-console.log('Hello World');
+const CloudmersiveConvertApiClient = require('cloudmersive-convert-api-client');
+
+const defaultClient = CloudmersiveConvertApiClient.ApiClient.instance;
+
+// Configure API key authorization: Apikey
+
+const Apikey = defaultClient.authentications['Apikey'];
+
+Apikey.apiKey = 'YOUR API KEY';
+
+// Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
+//Apikey.apiKeyPrefix = 'Token';
+
+const apiInstance = new CloudmersiveConvertApiClient.ConvertDocumentApi();
+
+// File | Input file to perform the operation on.
+const inputFile = '/path/to/file';
+
+const callback = function (error, data, response) {
+	if (error) {
+		console.error(error);
+	} else {
+		console.log('API called successfully. Returned data: ' + data);
+	}
+};
+
+apiInstance.convertDocumentRtfToHtml(inputFile, callback);
